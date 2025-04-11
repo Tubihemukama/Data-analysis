0672e78cf559c44bef1094efe92a45968ef6d00b
Tubihemukama <149370440+Tubihemukama@users.noreply.github.com>
2025-04-11 08:47:40 +0300

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -12,16 +12,9 @@ if upload is not None:
 
 
 
-    variable_list = dataset.columns
-    numeric_variable_list = dataset.select_dtypes(include='number').columns
-    def numeric_varaibles():
-        for var in numeric_variable_list:
-            mean_var = dataset[var].mean()
-            st.write(f"Mean of {var}:{mean_var:.2f}")
-    numeric_varaibles()
 
     non_numeric_variable_list = dataset.select_dtypes(include = ['object', 'string', 'category']).columns
-    def non_numeric_vaiables():
+    def non_numeric_variables():
         all_tables = []  # list to collect tables
         for var in non_numeric_variable_list:
             frequency_table = dataset[var].value_counts().reset_index()
@@ -35,7 +28,7 @@ if upload is not None:
         combined_table = pd.concat(all_tables, ignore_index=True)
         st.dataframe(combined_table)
             
-    non_numeric_vaiables()
+    non_numeric_variables()
 
    
         
